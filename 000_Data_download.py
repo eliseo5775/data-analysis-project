66fcbf97752cd046ed90f4a2276942f25b76df30
@eliseo5775 <eliseo.orellan@hotmail.com>
2020-08-30 15:46:53 -0500

    Descarga de csv

--- 000_Data_download.py
+++
@@ -1,3 +0,0 @@
-# Descarga de data de dataset de twetts http://galileoguzman.com/data/covid19_tweets.csv
-
-# Guardar en Dataset en carpeta
--- 001_Dias_transcurridos.py
+++ 001_Dias_transcurridos.py
@@ -1,3 +1,5 @@
 # Ejecuta una función que calcule cuantos días transcurridos han pasado hasta
 # el día que se ejecute, desde la primera vez que un usuario publicó un tweet
 # acerca del CoronaVirus.
+
+import pandas as pd 
