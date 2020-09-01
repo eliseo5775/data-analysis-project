0bbd19a935c99f2cc31da1a4a9100c1d3e1c8e95
@eliseo5775 <eliseo.orellan@hotmail.com>
2020-09-01 00:24:23 -0500

    date time

--- 000_ProyectoCompleto.py
+++ 000_ProyectoCompleto.py
@@ -1,15 +1,26 @@
 # Descarga de data de dataset de twetts http://galileoguzman.com/data/covid19_tweets.csv
 import requests
+import pandas as pd
+from datetime import date
 
 #df_web = requests.get('http://galileoguzman.com/data/covid19_tweets.csv')
 #print(df_web.status_code)
 
+FILENAME = 'Dataset/covid19_tweets.csv'
+df = pd.read_csv(FILENAME)
+
+print(df.head())
+
 # Ejecuta una función que calcule cuantos días transcurridos han pasado hasta
 # el día que se ejecute, desde la primera vez que un usuario publicó un tweet
 # acerca del CoronaVirus
-import pandas as pd
 
-FILENAME = 'Dataset/covid19_tweeets.csv'
+def diastranscurridos(df_input001):
+    current_date = date.today()
+    print(current_date)
+    return
 
-df = pd.read_csv(FILENAME)
+diastranscurridos(df)
+
+
 
