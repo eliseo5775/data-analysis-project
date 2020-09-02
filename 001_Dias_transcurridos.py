dc69c50f39d1fdeb1b4ffea64485cf1485e4c74e
@eliseo5775 <eliseo.orellan@hotmail.com>
2020-09-02 12:11:24 -0500

    calculo de dias

--- 001_Dias_transcurridos.py
+++ 001_Dias_transcurridos.py
@@ -15,18 +15,23 @@ df["user_created"] = df['user_created'].astype('datetime64[ns]')
 df["date"] = df['date'].astype('datetime64[ns]')
 
 #Exploramos la data
-#print(df.shape)
+print(df.shape)
 #print(df.head())
 #print(df.info())
 #print(df.iloc[1:5,5:13])
 
 
 def diastranscurridos(df_in001):
-    #current date
-    #current_date = date.today()
+    # limpiar el data set y quitar columnas feas
+    df_cl_in001 = df_in001.drop(columns=['user_followers','user_friends','user_favourites','user_description','user_created','source','is_retweet','text','user_verified','hashtags'])
     #date min por usuario
-    df_grp_min = df_in001.loc[df_in001.groupby('user_name').date.idxmin()]
-    print(f'DF agrupado \n {df_grp_min.head()}')
+    df_grp_min = df_cl_in001.loc[df_in001.groupby('user_name').date.idxmin()]
+    #agregamos el current date
+    current_date = date.today()
+    df_grp_min.insert(3,'Today_date',current_date)
+    #calculamos los dias entre dos fechas y agregamos columna
+    df_grp_min['dias_trans'] = (df_grp_min['Today_date']-df_grp_min['date']).days
+    print(f'DF agrupado \n {df_grp_min.head()} \n ,{df_grp_min.shape}')
     return()
 
 today = diastranscurridos(df)
